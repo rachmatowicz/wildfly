8dcdd93dde29641560c3bed1aacaa269a2ca08dc
Richard Achmatowicz <rachmato@redhat.com>
2025-08-22 17:07:01 -0400

    [WFLY-20091] Wire things together.

--- clustering/server/extension/src/main/java/org/wildfly/extension/clustering/server/provider/CacheServiceProviderRegistrarServiceInstallerFactory.java
+++ clustering/server/extension/src/main/java/org/wildfly/extension/clustering/server/provider/CacheServiceProviderRegistrarServiceInstallerFactory.java
@@ -20,7 +20,7 @@ import org.wildfly.subsystem.service.ServiceDependency;
 import org.wildfly.subsystem.service.ServiceInstaller;
 
 /**
- * Builds a cache-based {@link org.wildfly.clustering.server.provider.ServiceProviderRegistrar} service.
+ * Used to create a configured ServiceInstaller for the distrbuted variant of the ServiceProviderRegistrar clustering abstraction.
  * @author Paul Ferraro
  */
 public class CacheServiceProviderRegistrarServiceInstallerFactory<T> extends AbstractServiceProviderRegistrarServiceInstallerFactory<T> {
